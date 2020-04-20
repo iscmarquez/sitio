e30594a18ae88920550c1d754a3cf1ea4a1a7ea3
emarquez <iscmarquez@gmail.com>
2020-04-20 16:36:54 -0400

    Changement de serveur de contenu

--- script.js
+++ script.js
@@ -9,9 +9,9 @@ app.appendChild(logo);
 app.appendChild(container);
 
 var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open('GET', '//ghibliapi.herokuapp.com/films', true);
 request.onload = function () {
-
+xmlhttp.open("GET", "xmlhttp_info.txt", true);
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
